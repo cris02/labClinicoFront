7960a522c6c7c80d8ecc295b2d16805432ac63c7
SrKenn <ksperez15@gmail.com>
2022-05-23 18:20:13 -0600

    Botón para exportar a Excel funcional
    Agregado el boton para exportar los reportes a excel, se agregaron las dependencias de los modulos necesarios para la funcionalidad. Instalar dependencias para que no generen errores.

--- src/app/views/reportes/ej/ej.component.ts
+++ src/app/views/reportes/ej/ej.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 import {ServiceService} from "../Service/service.service";
 import {Factura} from "../Modelo/Factura";
+import * as FileSaver from 'file-saver';
+
 
 
 @Component({
@@ -20,7 +22,25 @@ export class EjComponent {
       .subscribe(data=>{
         this.facturas=data;
       })
+  }
 
+    exportExcel() {
+    if (this.facturas.length > 0) {
+      import("xlsx").then(xlsx => {
+        const worksheet = xlsx.utils.json_to_sheet(this.facturas);
+        const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+        const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+        this.saveAsExcelFile(excelBuffer, "ExportExcel");
+      });
+    }
+  }
+  saveAsExcelFile(buffer: any, fileName: string): void {
+    let EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+    let EXCEL_EXTENSION = '.xlsx';
+    const data: Blob = new Blob([buffer], {
+      type: EXCEL_TYPE
+    });
+    FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
   }
 
 }
